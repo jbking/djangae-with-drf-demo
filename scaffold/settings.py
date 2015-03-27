07c1075d37bce62f537eb7e864cc4b9860d0c1b9
Nicola Valentini <nicola.valentini@gmail.com>
2015-03-27 11:23:20 +0000

    Adding some useful CSP settings. #31

--- scaffold/settings.py
+++ scaffold/settings.py
@@ -119,5 +119,14 @@ STATIC_URL = '/static/'
 if DEBUG:
     CSP_STYLE_SRC = ("'self'", "'unsafe-inline'")
 
+# sensible default CPS settings, feel free to modify them
+CSP_DEFAULT_SRC = ("'self'", "*.gstatic.com")
+CSP_STYLE_SRC = ("'self'", "fonts.googleapis.com", "*.gstatic.com")
+CSP_FONT_SRC = ("'self'", "themes.googleusercontent.com", "*.gstatic.com")
+CSP_FRAME_SRC = ("'self'", "www.google.com", "www.youtube.com", "accounts.google.com", "apis.google.com", "plus.google.com")
+CSP_SCRIPT_SRC = ("'self'", "*.googleanalytics.com", "*.google-analytics.com", "ajax.googleapis.com")
+CSP_IMG_SRC = ("'self'", "data:", "s.ytimg.com", "*.googleusercontent.com", "*.gstatic.com")
+CSP_CONNECT_SRC = ("'self'", "plus.google.com", "www.google-analytics.com")
+
 
 from djangae.contrib.gauth.settings import *
